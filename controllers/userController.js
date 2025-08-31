c9f9e4dddf55e2805612bb9adb8676e238fc218e
Meku-nint <mw908334@gmail.com>
2025-08-31 20:10:51 +0300

    commited

---
+++ controllers/userController.js
@@ -0,0 +1,5 @@
+import express from "express"
+import {Order}from "../models/models.js";
+export const newOrders =async(req,res)=>{
+    
+}
--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 import express from "express";
 import connectDB from "./config/db.js";
-import orderRoute from '.controllers/userRoute.js';
 import dotenv from "dotenv";
+import useRoutes from "./routes/useRoutes.js";
 dotenv.config();
 connectDB();
 const app=express();
 app.use(cors());
 app.use(express.json());
-
-// Routes
-app.use("/api/orders", orderRoutes);
+app.use("/api",useRoutes);
 const PORT =process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log(`The server running on port ${PORT}`);

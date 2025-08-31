9e31acb93af0f9829bf67f1911e1dfc4cc007c4c
Meku-nint <mw908334@gmail.com>
2025-08-31 10:08:39 +0300

    db connected

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const connectDB=async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI,{
+               useNewUrlParser: true,
+               useUnifiedTopology: true
+        });
+        console.log("Mongo is connected");
+    } catch (error) {
+        console.error("❌ MongoDB Connection Failed:", error.message);
+    process.exit(1);
+
+    }
+}

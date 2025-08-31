adce9a7d55b9ba22862838da56489d47c743b0ef
Meku-nint <mw908334@gmail.com>
2025-08-31 12:20:30 +0300

    commited

---
+++ models/order.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const orderSchema=new mongoose.Schema({
+    departure:{
+        type:String,required:true
+    },
+    destination:{
+        type:String,required:true
+    }, 
+     weight:{
+        type:String,required:true
+    },
+    phone:{
+        type:String,required:true
+    },
+     detail:{
+        type:String,required:true
+    }
+})
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+connectDB();
 const app=express();
 const PORT =process.env.PORT||5000;
 app.listen(PORT,()=>{

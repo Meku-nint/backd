adfc041855efdf67fc0e02d65846e5b81a770137
Meku-nint <mw908334@gmail.com>
2025-09-01 16:06:43 +0300

    commited

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import express from "express"
-import {Order}from "../models/models.js";
+import Order from "../models/models.js";
 export const newOrders =async(req,res)=>{
-    
+    const {departure,destination,weight,phone,detail}=req.body;
+    console.log(departure,destination,weight,phone,detail);
 }
--- routes/useRoutes.js
+++ routes/useRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-
+import { newOrders } from "../controllers/userController.js";
 const router=Router();
 router.post('/orders',newOrders);
 export default router;

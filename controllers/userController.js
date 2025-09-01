6d084972ef9ba30c96e53709492132a7bfc0d97f
Meku-nint <mw908334@gmail.com>
2025-09-01 20:54:25 +0300

    commited

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,5 +2,11 @@ import express from "express"
 import Order from "../models/models.js";
 export const newOrders =async(req,res)=>{
     const {departure,destination,weight,phone,detail}=req.body;
-    console.log(departure,destination,weight,phone,detail);
+    try {
+        if(!departure ||!destination||!weight||!phone||!detail){
+            res.status(404)
+        }
+    } catch (error) {
+        
+    }
 }

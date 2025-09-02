784ab8ad034e1e1c9bfe7449080af24a104d09a1
Meku-nint <mw908334@gmail.com>
2025-09-02 08:45:49 +0300

    commited

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,9 +4,22 @@ export const newOrders =async(req,res)=>{
     const {departure,destination,weight,phone,detail}=req.body;
     try {
         if(!departure ||!destination||!weight||!phone||!detail){
-            res.status(404)
+            return res.status(400).json({
+                error:"All fields are required",
+                missing:{
+                    departure:!departure,
+                    destination:!destination,
+                    weight:!weight,
+                    phone:!phone,
+                    detail:!detail,
+                
+                }
+            });
         }
+        const order =new Order ({departure, destination, weight, phone, detail });
+        await order.save();
+        return res.status(201).json({message:"order is successful submitted"});
     } catch (error) {
-        
+        return res.status(500).json({error:error.message});
     }
 }
